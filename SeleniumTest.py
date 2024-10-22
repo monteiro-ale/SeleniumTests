38baf2b6c40f7dd0273eb940a3f6029f6eb8c5a9
monteiro-ale <monteiro.tec32@gmail.com>
2024-10-21 23:02:36 -0300

    Add invoice tests.

--- SeleniumTest.py
+++ SeleniumTest.py
@@ -23,6 +23,13 @@ class TestSeuBarriga(unittest.TestCase):
 				new_login = f"teste{data_hora_str}@teste.com"
 				return new_login
 
+		# Gera uma nova fatura aleatoria
+		def generate_new_invoice(self):
+				now = datetime.now()
+				data_hora_str = now.strftime("%Y%m%d_%H%M%S")
+				new_invoice_name = f"Fatura do Aluguel - Vencimento {data_hora_str}"
+				return new_invoice_name
+
 		# Testa o cadastro com novo login
 		def test_register_and_login(self):
 				login_new_user = self.generate_new_user()
@@ -34,45 +41,70 @@ class TestSeuBarriga(unittest.TestCase):
 				)
 				cadastro.click()
 
-				nome = self.driver.find_element(By.ID, "nome")
+				nome = WebDriverWait(self.driver, 10).until(
+						EC.presence_of_element_located((By.ID, "nome"))
+				)
 				nome.send_keys("Alexandre")
 				time.sleep(1)
-
 				email = self.driver.find_element(By.ID, "email")
 				email.send_keys(login_new_user)
 				time.sleep(1)
-
 				senha = self.driver.find_element(By.ID, "senha")
 				senha.send_keys(password_new_user)
-
+				time.sleep(1)
 				submit = self.driver.find_element(By.XPATH, '/html/body/div[2]/form/input')
 				submit.click()
-				time.sleep(1)
 
 				try:
-						message = self.driver.find_element(By.XPATH, '/html/body/div[1]')
+						message = WebDriverWait(self.driver, 10).until(
+								EC.presence_of_element_located((By.XPATH, '/html/body/div[1]'))
+						)
 						print(f"Usuário {login_new_user} cadastrado com sucesso: {message.text}")
 						print('Passando para tela de login')
 						self.login_user(login_new_user, password_new_user)
-				except:
-						print('Falha no cadastro do usuário')
+						time.sleep(2)
+						self.add_invoice()
+				except Exception as e:
+						print('Falha no cadastro do usuário:', str(e))
 
 		def login_user(self, user, password):
 				email_field = self.driver.find_element(By.ID, "email")
 				email_field.send_keys(user)
-
+				time.sleep(1)
 				password_field = self.driver.find_element(By.ID, "senha")
 				password_field.send_keys(password)
-
+				time.sleep(1)
 				login_button = self.driver.find_element(By.XPATH, "//button[@type='submit']")
 				login_button.click()
-				time.sleep(3)
 
 				try:
-						message = self.driver.find_element(By.XPATH, "//div[@class='alert alert-success']")
+						message = WebDriverWait(self.driver, 10).until(
+								EC.presence_of_element_located((By.XPATH, "//div[@class='alert alert-success']"))
+						)
 						print("Login bem-sucedido:", message.text)
-				except:
-						print("Falha no login.")
+				except Exception as e:
+						print("Falha no login:", str(e))
+
+		def add_invoice(self):
+				new_invoice = self.generate_new_invoice()
+				nav_bar = self.driver.find_element(By.XPATH, '//*[@id="navbar"]/ul/li[2]/a')
+				nav_bar.click()
+				add = self.driver.find_element(By.XPATH, '//*[@id="navbar"]/ul/li[2]/ul/li[1]/a')
+				add.click()
+
+				invoice_name = self.driver.find_element(By.ID,"nome")
+				invoice_name.send_keys(new_invoice)
+
+				save = self.driver.find_element(By.XPATH, '/html/body/div[2]/form/div[2]/button')
+				save.click()
+
+				try:
+						message = WebDriverWait(self.driver, 10).until(
+								EC.presence_of_element_located((By.XPATH, "//div[@class='alert alert-success']"))
+						)
+						print(f'Conta adicionada com sucesso! {message.text}')
+				except Exception as e:
+						print('Erro ao adicionar nova conta:', str(e))
 
 if __name__ == "__main__":
 		unittest.main()
